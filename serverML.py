8bd773823c17e91b123820f86424ec49b673cc2f
Eric <eric_manuel14@hotmail.com>
2024-08-27 19:47:57 -0600

    Correcion

--- serverML.py
+++ serverML.py
@@ -28,7 +28,7 @@ def predictjson():
     #Predecir utilizando la entrada y el modelo
     result = dt.predict(inputData.reshape(1,-1))
     #Enviar respuesta
-    return jsonify({'Prediction:': str(result[0])})
+    return jsonify({'prediction:': str(result[0])})
 
 if __name__ == '__main__':
     server.run (debug=False, host='0.0.0.0',port=8080)
